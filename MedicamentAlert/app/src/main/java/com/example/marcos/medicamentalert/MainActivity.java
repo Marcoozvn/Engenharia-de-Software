ef9a857866bd638a76f5d85ba94bb4bbb625c906
Marcos Cesar <marcos.cesar@ccc.ufcg.edu.br>
2017-06-11 10:29:08 -0300

    Adicionando Spinners

--- MedicamentAlert/app/src/main/java/com/example/marcos/medicamentalert/MainActivity.java
+++ MedicamentAlert/app/src/main/java/com/example/marcos/medicamentalert/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.marcos.medicamentalert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinnerFrequencia = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapterSpinnerFrequencia = ArrayAdapter.createFromResource(this,
+                R.array.frequencia_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapterSpinnerFrequencia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinnerFrequencia.setAdapter(adapterSpinnerFrequencia);
+
+        Spinner spinnerDosagem = (Spinner) findViewById(R.id.spinnerDosagem);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapterSpinnerDosagem = ArrayAdapter.createFromResource(this,
+                R.array.dosagem_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapterSpinnerDosagem.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinnerDosagem.setAdapter(adapterSpinnerDosagem);
     }
+
+
 }
