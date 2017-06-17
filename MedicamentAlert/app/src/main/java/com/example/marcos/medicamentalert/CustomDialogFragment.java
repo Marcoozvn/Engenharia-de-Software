7d7704a526f33438c08f0656db8395026b870422
Marcos Cesar <marcos.cesar@ccc.ufcg.edu.br>
2017-06-17 19:27:02 -0300

    Ajustando TimePicker com DialogFragment

---
+++ MedicamentAlert/app/src/main/java/com/example/marcos/medicamentalert/CustomDialogFragment.java
@@ -0,0 +1,28 @@
+package com.example.marcos.medicamentalert;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Marcos on 17/06/2017.
+ */
+
+public class CustomDialogFragment extends DialogFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.timepicker, container);
+        return view;
+    }
+}
--- MedicamentAlert/app/src/main/java/com/example/marcos/medicamentalert/MainActivity.java
+++ MedicamentAlert/app/src/main/java/com/example/marcos/medicamentalert/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.marcos.medicamentalert;
 
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.Spinner;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Medicamento> medicamentos = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,23 +26,55 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner spinnerFrequencia = (Spinner) findViewById(R.id.spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
+        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapterSpinnerFrequencia = ArrayAdapter.createFromResource(this,
                 R.array.frequencia_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+        // Specify the layout to use when the list of choices appears
         adapterSpinnerFrequencia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
+        // Apply the adapter to the spinner
         spinnerFrequencia.setAdapter(adapterSpinnerFrequencia);
 
         Spinner spinnerDosagem = (Spinner) findViewById(R.id.spinnerDosagem);
-// Create an ArrayAdapter using the string array and a default spinner layout
+        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapterSpinnerDosagem = ArrayAdapter.createFromResource(this,
                 R.array.dosagem_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+        // Specify the layout to use when the list of choices appears
         adapterSpinnerDosagem.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
+        // Apply the adapter to the spinner
         spinnerDosagem.setAdapter(adapterSpinnerDosagem);
+
+        Button salvar = (Button) findViewById(R.id.salvar);
+        salvar.setOnClickListener(salvarOnClickListener);
+
+        TextClock textClock = (TextClock) findViewById(R.id.textClock);
+        textClock.setOnClickListener(dialogFragmentOnClickListener);
     }
 
+    private View.OnClickListener dialogFragmentOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            CustomDialogFragment dialogFragment = new CustomDialogFragment();
+            dialogFragment.show(fragmentTransaction, "");
+        }
+    };
+
+    private View.OnClickListener salvarOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            EditText viewNomeMedicamento = (EditText) findViewById(R.id.form_nomeMedicamento);
+            EditText viewNomeMedico = (EditText) findViewById(R.id.form_nomeMedico);
+            EditText viewDosagem = (EditText) findViewById(R.id.form_quantidadeDosagem);
+            Spinner spinnerDosagem = (Spinner) findViewById(R.id.spinnerDosagem);
+
+            String nomeMedicamento = viewNomeMedicamento.getText().toString();
+            String nomeMedico = viewNomeMedico.getText().toString();
+            String dosagem = viewDosagem.getText().toString();
+            String dosagemMetrica = spinnerDosagem.getSelectedItem().toString();
+
+            Medicamento medicamento = new Medicamento(nomeMedicamento, nomeMedico, Integer.valueOf(dosagem), dosagemMetrica);
+            medicamentos.add(medicamento);
+        }
+    };
 
 }
